6996b866a58208b8e42bb770bb05ea50e2357c62
Jordan van Beijnhem <jordan.van.beijnhem@hva.nl>
2017-11-21 17:43:36 +0100

    + Added correct \"Gevonden Bagage\" scene (previous one was out of date) + Fixed bug where "Rapportage" scene would not open

--- src/main/java/fys/luggagem/GevondenBagageController.java
+++ src/main/java/fys/luggagem/GevondenBagageController.java
@@ -1,52 +1,65 @@
 package fys.luggagem;
 
+import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-
 /**
  * @author Mees Sour
  */
-public class GevondenBagageController implements Initializable {  
-    
+public class GevondenBagageController implements Initializable {
+
+    Data data;
+
     @FXML
-    private Label statusMessage;       
-            
+    private Label statusMessage;
+
     @FXML
     private TextField DatumDag;
-    
+
     @FXML
     private TextField DatumMaand;
-    
+
     @FXML
     private TextField DatumJaar;
-    
+
     @FXML
     private TextField TijdUur;
-    
+
     @FXML
     private TextField TijdMinuut;
-    
+
     @FXML
     private AnchorPane rootPane;
-    
+
     @FXML
     private Label nameLabel;
-    
+
     @FXML
-    private void opslaan(ActionEvent event){
+    private void handleSaveAction(ActionEvent event) {
         statusMessage.setText("Succesvol opgeslagen naar database");
         System.out.println("GELUKT!");
-    }           
-    
+    }
+
+    @FXML
+    private void handleCloseAction(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/HomeScreenFXML.fxml"));
+        Parent root = (Parent) loader.load();
+        HomeScreenFXMLController controller = loader.getController();
+        controller.initData(data);
+        data.getScene().setRoot(root);
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         SimpleDateFormat dateFormatDag = new SimpleDateFormat("dd");
@@ -55,15 +68,14 @@ public class GevondenBagageController implements Initializable {
         DatumMaand.setText(dateFormatMaand.format(new Date()));
         SimpleDateFormat dateFormatJaar = new SimpleDateFormat("yyyy");
         DatumJaar.setText(dateFormatJaar.format(new Date()));
-        
+
         SimpleDateFormat timeFormatUur = new SimpleDateFormat("HH");
         TijdUur.setText(timeFormatUur.format(new Date()));
         SimpleDateFormat timeFormatMinuut = new SimpleDateFormat("mm");
         TijdMinuut.setText(timeFormatMinuut.format(new Date()));
-    }    
-
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
     }
-    
+
+    public void initData(Data mainData) {
+        data = mainData;
+    }
 }
