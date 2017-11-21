35d63e4e2de067201d97b7169ea7fbb6edcbe60f
Mees Sour <meessour@gmail.com>
2017-11-21 16:14:05 +0100

    Redid screen -nonfunctional-

--- src/main/java/fys/luggagem/GevondenBagageController.java
+++ src/main/java/fys/luggagem/GevondenBagageController.java
@@ -1,56 +1,52 @@
 package fys.luggagem;
 
-import java.io.IOException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+
 /**
  * @author Mees Sour
  */
-public class GevondenBagageController implements Initializable {
-
-    Data data;
+public class GevondenBagageController implements Initializable {  
     
     @FXML
-    private Label statusMessage;
-
+    private Label statusMessage;       
+            
     @FXML
     private TextField DatumDag;
-
+    
     @FXML
     private TextField DatumMaand;
-
+    
     @FXML
     private TextField DatumJaar;
-
+    
     @FXML
     private TextField TijdUur;
-
+    
     @FXML
     private TextField TijdMinuut;
-
+    
     @FXML
     private AnchorPane rootPane;
-
+    
     @FXML
     private Label nameLabel;
-
+    
     @FXML
-    private void opslaan(ActionEvent event) {
+    private void opslaan(ActionEvent event){
         statusMessage.setText("Succesvol opgeslagen naar database");
         System.out.println("GELUKT!");
-    }
-
+    }           
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         SimpleDateFormat dateFormatDag = new SimpleDateFormat("dd");
@@ -59,23 +55,15 @@ public class GevondenBagageController implements Initializable {
         DatumMaand.setText(dateFormatMaand.format(new Date()));
         SimpleDateFormat dateFormatJaar = new SimpleDateFormat("yyyy");
         DatumJaar.setText(dateFormatJaar.format(new Date()));
-
+        
         SimpleDateFormat timeFormatUur = new SimpleDateFormat("HH");
         TijdUur.setText(timeFormatUur.format(new Date()));
         SimpleDateFormat timeFormatMinuut = new SimpleDateFormat("mm");
         TijdMinuut.setText(timeFormatMinuut.format(new Date()));
+    }    
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
     }
     
-    @FXML
-    private void handleCloseAction(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/HomeScreenFXML.fxml"));
-        Parent root = (Parent) loader.load();
-        HomeScreenFXMLController controller = loader.getController();
-        controller.initData(data);
-        data.getScene().setRoot(root);
-    }
-
-    public void initData(Data mainData) {
-        data = mainData;
-    }
 }
