64cdf3f381c20097c162b4418621bb86704ff931
Jordan van Beijnhem <jordan.van.beijnhem@hva.nl>
2017-11-27 12:46:04 +0100

    changed layout

--- src/main/java/fys/luggagem/GevondenBagageController.java
+++ src/main/java/fys/luggagem/GevondenBagageController.java
@@ -17,92 +17,92 @@ import javafx.scene.layout.AnchorPane;
  */
 public class GevondenBagageController implements Initializable {
     
-    private Data data = MainApp.getData();
-
-    @FXML
-    private Label statusMessage;
-
-    @FXML
-    private TextField datumDag;
-
-    @FXML
-    private TextField datumMaand;
-
-    @FXML
-    private TextField datumJaar;
-
-    @FXML
-    private TextField tijdUur;
-
-    @FXML
-    private TextField tijdMinuut;
-
-    @FXML
-    private TextField labelNummer;
-
-    @FXML
-    private TextField vluchtNummer;
-
-    @FXML
-    private TextField bestemming;
-
-    @FXML
-    private TextField voorNaam;
-
-    @FXML
-    private TextField tussenvoegsel;
-
-    @FXML
-    private TextField achterNaam;
-
-    @FXML
-    private TextField luchthaven;
-
-    @FXML
-    private TextField lostAndFoundId;
-
-    @FXML
-    private TextField bagageType;
-
-    @FXML
-    private TextField bagageMerk;
-
-    @FXML
-    private TextField bagageKleur;
-
-    @FXML
-    private TextField bagageBijzondereKenmerken;
-
-    @FXML
-    private AnchorPane rootPane;
-
-    @FXML
-    private Label nameLabel;
-
+//    private Data data = MainApp.getData();
+//
+//    @FXML
+//    private Label statusMessage;
+//
+//    @FXML
+//    private TextField datumDag;
+//
+//    @FXML
+//    private TextField datumMaand;
+//
+//    @FXML
+//    private TextField datumJaar;
+//
+//    @FXML
+//    private TextField tijdUur;
+//
+//    @FXML
+//    private TextField tijdMinuut;
+//
+//    @FXML
+//    private TextField labelNummer;
+//
+//    @FXML
+//    private TextField vluchtNummer;
+//
+//    @FXML
+//    private TextField bestemming;
+//
+//    @FXML
+//    private TextField voorNaam;
+//
+//    @FXML
+//    private TextField tussenvoegsel;
+//
+//    @FXML
+//    private TextField achterNaam;
+//
+//    @FXML
+//    private TextField luchthaven;
+//
+//    @FXML
+//    private TextField lostAndFoundId;
+//
+//    @FXML
+//    private TextField bagageType;
+//
+//    @FXML
+//    private TextField bagageMerk;
+//
+//    @FXML
+//    private TextField bagageKleur;
+//
+//    @FXML
+//    private TextField bagageBijzondereKenmerken;
+//
+//    @FXML
+//    private AnchorPane rootPane;
+//
+//    @FXML
+//    private Label nameLabel;
+//
     @FXML
     private void handleCloseAction(ActionEvent event) throws IOException {
         MainApp.setScene(this.getClass().getResource("/fxml/HomeScreenFXML.fxml"));
     }
-
-    @FXML
-    private void opslaan(ActionEvent event) {
-        statusMessage.setText("Succesvol opgeslagen naar database");
-        System.out.println("GELUKT!");
-    }
+//
+//    @FXML
+//    private void opslaan(ActionEvent event) {
+//        statusMessage.setText("Succesvol opgeslagen naar database");
+//        System.out.println("GELUKT!");
+//    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        SimpleDateFormat dateFormatDag = new SimpleDateFormat("dd");
-        datumDag.setText(dateFormatDag.format(new Date()));
-        SimpleDateFormat dateFormatMaand = new SimpleDateFormat("MM");
-        datumMaand.setText(dateFormatMaand.format(new Date()));
-        SimpleDateFormat dateFormatJaar = new SimpleDateFormat("yyyy");
-        datumJaar.setText(dateFormatJaar.format(new Date()));
-
-        SimpleDateFormat timeFormatUur = new SimpleDateFormat("HH");
-        tijdUur.setText(timeFormatUur.format(new Date()));
-        SimpleDateFormat timeFormatMinuut = new SimpleDateFormat("mm");
-        tijdMinuut.setText(timeFormatMinuut.format(new Date()));
+//        SimpleDateFormat dateFormatDag = new SimpleDateFormat("dd");
+//        datumDag.setText(dateFormatDag.format(new Date()));
+//        SimpleDateFormat dateFormatMaand = new SimpleDateFormat("MM");
+//        datumMaand.setText(dateFormatMaand.format(new Date()));
+//        SimpleDateFormat dateFormatJaar = new SimpleDateFormat("yyyy");
+//        datumJaar.setText(dateFormatJaar.format(new Date()));
+//
+//        SimpleDateFormat timeFormatUur = new SimpleDateFormat("HH");
+//        tijdUur.setText(timeFormatUur.format(new Date()));
+//        SimpleDateFormat timeFormatMinuut = new SimpleDateFormat("mm");
+//        tijdMinuut.setText(timeFormatMinuut.format(new Date()));
     }
 
 }
