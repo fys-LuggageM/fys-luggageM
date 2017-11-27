4fa6f6bbebce306fdade6af66b682be716bd0279
Jordan van Beijnhem <jordan.van.beijnhem@hva.nl>
2017-11-27 20:13:43 +0100

    Integrated "Gevonden Bagage" en "Gebruikersbeheer" into homescreen

--- src/main/java/fys/luggagem/FXMLDocumentController.java
+++ src/main/java/fys/luggagem/FXMLDocumentController.java
@@ -5,9 +5,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -43,10 +41,10 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private Button dutchButton;
-    
+
     @FXML
     private Button englishButton;
-    
+
     @FXML
     private TextField username;
 
---
+++ src/main/java/fys/luggagem/HomeScreenContentFXMLController.java
@@ -0,0 +1,68 @@
+package fys.luggagem;
+
+import java.awt.event.ActionListener;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javax.swing.Timer;
+
+/**
+ * FXML Controller class
+ *
+ * @author jordan
+ */
+public class HomeScreenContentFXMLController implements Initializable {
+
+    Data data = MainApp.getData();
+    Timer t;
+    
+    @FXML
+    private Label timeLabel;
+    
+    @FXML
+    private Label greetingLabel;
+    
+    @FXML
+    private Label dateLabel;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        t = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        timeLabel.setText(sdf.format(new Date()));
+                        Calendar rightNow = Calendar.getInstance();
+                        int hour = rightNow.get(Calendar.HOUR_OF_DAY);
+                        if (hour >= 0 && hour < 6) {
+                            greetingLabel.setText(data.getResourceBundle().getString("greetingsNight") + ", "
+                                    + data.getName().substring(0, data.getName().indexOf(" ")));
+                        } else if (hour >= 6 && hour < 12) {
+                            greetingLabel.setText(data.getResourceBundle().getString("greetingsMorning") + ", "
+                                    + data.getName().substring(0, data.getName().indexOf(" ")));
+                        } else if (hour >= 12 && hour < 18) {
+                            greetingLabel.setText(data.getResourceBundle().getString("greetingsAfternoon") + ", "
+                                    + data.getName().substring(0, data.getName().indexOf(" ")));
+                        } else if (hour >= 18 && hour < 24) {
+                            greetingLabel.setText(data.getResourceBundle().getString("greetingsEvening") + ", "
+                                    + data.getName().substring(0, data.getName().indexOf(" ")));
+                        }
+                    }
+                });
+            }
+        });
+        t.start();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        dateLabel.setText(dateFormat.format(new Date())); 
+    }    
+    
+}
