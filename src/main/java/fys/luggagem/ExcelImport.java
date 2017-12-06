91327008b6eb2dcf048ea40441cfca0e7fa39309
Mees Sour <meessour@gmail.com>
2017-12-06 20:45:48 +0100

    Almost finished Found Luggage.
    Getting the String value date from the excel sheet and converting it to a Date value is not working yet.
    Weird bug with the String time when getting the value from an excel sheet, it makes the time 1 hour earlier than what is listed in the sheet.
    There might be some work needed for the layout from the FMXL file.

--- src/main/java/fys/luggagem/ExcelImport.java
+++ src/main/java/fys/luggagem/ExcelImport.java
@@ -3,7 +3,12 @@ package fys.luggagem;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import org.apache.poi.ss.usermodel.Cell;
@@ -28,7 +33,27 @@ public class ExcelImport {
     private String luggageType;
     private String brand;
     private String flightNr;
+    private String luggageTag;
+    private String locationFound;
+    private String primaryColor;
+    private String secondaryColor;
 
+    private String luggageSize;
+    private String luggageSizeHeigth;
+    private String luggageSizeWidth;
+    private String luggageSizeDepth;
+
+    private String luggageWeight;
+
+    private String travellerNameAndCity;
+    private String travellerNameAndCityName;
+    private String travellerNameAndCityCity;
+
+    private String comments;
+
+//    private static int beginAtRow = 0;
+//    private static int a = 1;
+//    private static String temp;
     public static List<ExcelImport> importFoundLuggageFromExcel(String fileName) throws IOException {
         List<ExcelImport> luggageList = new ArrayList<>();
 
@@ -36,7 +61,18 @@ public class ExcelImport {
         Sheet firstSheet = workbook.getSheetAt(0);
         int numOfColumns = firstSheet.getRow(20).getPhysicalNumberOfCells();
 
-        for (int i = 5; i < firstSheet.getLastRowNum(); i++) {
+//        while (beginAtRow == 0) {
+//            
+//            Row row = firstSheet.getRow(a);
+//            Cell cell = row.getCell(0);
+//            
+//            setTemp(cellToString(cell));
+//            if (temp == "Registration nr") {
+//                beginAtRow = a;
+//            }
+//            a++;
+//        }
+        for (int i = 4; i < firstSheet.getLastRowNum(); i++) {
             ExcelImport importedLugage = new ExcelImport();
             Row row = firstSheet.getRow(i);
             for (int j = 0; j < numOfColumns; j++) {
@@ -46,6 +82,7 @@ public class ExcelImport {
                         importedLugage.setRegistrationNr(cellToString(cell));
                         break;
                     case 1:
+//                        importedLugage.setDateFound(cellToString(cell));
                         break;
                     case 2:
                         importedLugage.setTimeFound(cellToString(cell));
@@ -58,11 +95,149 @@ public class ExcelImport {
                         break;
                     case 5:
                         importedLugage.setFlightNr(cellToString(cell));
+                        break;
+                    case 6:
+                        importedLugage.setLuggageTag(cellToString(cell));
+                        break;
+                    case 7:
+                        importedLugage.setLocationFound(cellToString(cell));
+                        break;
+                    case 8:
+                        importedLugage.setPrimaryColor(cellToString(cell));
+                        break;
+                    case 9:
+                        importedLugage.setSecondaryColor(cellToString(cell));
+                        break;
+                    case 10:
+                        importedLugage.setLuggageSize(cellToString(cell));
+                        break;
+                    case 11:
+                        importedLugage.setLuggageWeight(cellToString(cell));
+                        break;
+                    case 12:
+                        importedLugage.setTravellerNameAndCity(cellToString(cell));
+                        break;
+                    case 13:
+                        importedLugage.setComments(cellToString(cell));
                 }
             }
             luggageList.add(importedLugage);
         }
         return luggageList;
+    }
+
+    public String getLuggageTag() {
+        return luggageTag;
+    }
+
+    public void setLuggageTag(String num) {
+        luggageTag = num;
+    }
+
+    public String getLocationFound() {
+        return locationFound;
+    }
+
+    public void setLocationFound(String num) {
+        locationFound = num;
+    }
+
+    public String getPrimaryColor() {
+        return primaryColor;
+    }
+
+    public void setPrimaryColor(String num) {
+        primaryColor = num;
+    }
+
+    public String getSecondaryColor() {
+        return secondaryColor;
+    }
+
+    public void setSecondaryColor(String num) {
+        secondaryColor = num;
+    }
+
+    public void setLuggageSize(String num) {
+        if (num != null) {
+            int i = 0;
+            String[] parts = new String[3];
+
+            for (String retval : num.split("x")) {
+                parts[i] = retval;
+                i++;
+            }
+            String heigth = parts[0];
+            String width = parts[1];
+            String depth = parts[2];
+
+            luggageSizeHeigth = heigth;
+            luggageSizeWidth = width;
+            luggageSizeDepth = depth;
+        } else {
+            luggageSizeHeigth = "";
+            luggageSizeWidth = "";
+            luggageSizeDepth = "";
+        }
+    }
+
+    public String getLuggageSizeHeigth() {
+        return luggageSizeHeigth;
+    }
+
+    public String getLuggageSizeWidth() {
+        return luggageSizeWidth;
+    }
+
+    public String getLuggageSizeDepth() {
+        return luggageSizeDepth;
+    }
+
+    public String getLuggageWeight() {
+        return luggageWeight;
+    }
+
+    public void setLuggageWeight(String num) {
+        luggageWeight = num;
+    }
+
+    public void setTravellerNameAndCity(String num) {
+
+        if (num != null) {
+            int i = 0;
+            String[] parts = new String[2];
+
+            for (String retval : num.split(", ")) {
+                parts[i] = retval;
+                i++;
+            }
+            String name = parts[0];
+            String city = parts[1];
+
+            travellerNameAndCityName = name;
+            travellerNameAndCityCity = city;
+
+        } else {
+            travellerNameAndCityName = "";
+            travellerNameAndCityCity = "";
+        }
+
+    }
+
+    public String getTravellerNameAndCityName() {
+        return travellerNameAndCityName;
+    }
+
+    public String getTravellerNameAndCityCity() {
+        return travellerNameAndCityCity;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String num) {
+        comments = num;
     }
 
     private static String cellToString(Cell cell) {
@@ -93,6 +268,7 @@ public class ExcelImport {
     }
 
     private void setRegistrationNr(String num) {
+        num = num.replace("/", "");
         registrationNr = num;
     }
 
@@ -100,10 +276,10 @@ public class ExcelImport {
         return registrationNr;
     }
 
-    private void setDateFound(String num) {
+    private void setDateFound(String num) {      
         dateFound = num;
     }
-
+    
     public String getDateFound() {
         return dateFound;
     }
@@ -133,9 +309,9 @@ public class ExcelImport {
     }
 
     private void setFlightNr(String num) {
-        flightNr = num; 
+        flightNr = num;
     }
-    
+
     public String getFlightNr() {
         return flightNr;
     }
