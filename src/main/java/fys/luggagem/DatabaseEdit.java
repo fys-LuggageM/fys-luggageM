b9b13df22001c2eadc01082ac8cd02acff86e6c2
Valentijn Vermeij <valentijn.vermeij@hva.nl>
2017-12-10 17:11:09 +0100

    Can now do proper readouts of table in edit screen
    Commiting the changes isn't there though yet...

--- src/main/java/fys/luggagem/DatabaseEdit.java
+++ src/main/java/fys/luggagem/DatabaseEdit.java
@@ -6,15 +6,20 @@ package fys.luggagem;
  * and open the template in the editor.
  */
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -36,8 +41,7 @@ public class DatabaseEdit implements Initializable {
     private TextField color1Field;
     @FXML
     private TextField colorField2;
-    @FXML
-    private TextField IATAField;
+
     @FXML
     private TextField brandField;
     @FXML
@@ -60,11 +64,24 @@ public class DatabaseEdit implements Initializable {
     private TextField betweenNameField;
     @FXML
     private TextField lastNameField;
+    @FXML
+    private TextField flightNrField;
+    @FXML
+    private TextField luggageTypeField;
+    @FXML
+    private ComboBox<String> airportField;
+    @FXML
+    private TextField customerIDField;
+
+    private ObservableList<String> Airports = FXCollections.observableArrayList();
+    @FXML
+    private TextField labelNrField;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         registrationNumber = MainApp.data.getLuggageRegistrationNumber();
         setCurrentLuggageFields();
+        setupAiportBox();
     }
 
     private void setCurrentLuggageFields() {
@@ -88,11 +105,10 @@ public class DatabaseEdit implements Initializable {
                 } catch (NullPointerException e) {
                 }
 
-                try {
-                    IATAField.setText(result.getString("airport_IATA"));
-                } catch (NullPointerException e) {
-                }
-
+                //try {
+                //IATAField.setText(result.getString("airport_IATA"));
+                //} catch (NullPointerException e) {
+                //}
                 try {
                     destinationField.setText(result.getString("destination"));
 
@@ -120,22 +136,42 @@ public class DatabaseEdit implements Initializable {
 
                 try {
                     locationFoundField.setText(result.getString("location_found"));
-                } catch (Exception e) {
+                } catch (NullPointerException e) {
                 }
 
                 try {
-                    firstNameField.setText("customer_firstname");
-                } catch (Exception e) {
+                    firstNameField.setText(result.getString("customer_firstname"));
+                } catch (NullPointerException e) {
                 }
 
                 try {
-                    betweenNameField.setText("customer_preposition");
-                } catch (Exception e) {
+                    betweenNameField.setText(result.getString("customer_preposition"));
+                } catch (NullPointerException e) {
                 }
 
                 try {
-                    lastNameField.setText("customer_lastname");
-                } catch (Exception e) {
+                    lastNameField.setText(result.getString("customer_lastname"));
+                } catch (NullPointerException e) {
+                }
+
+                try {
+                    flightNrField.setText(result.getString("flightnr"));
+                } catch (NullPointerException e) {
+                }
+
+                try {
+                    labelNrField.setText(result.getString("labelnr"));
+                } catch (NullPointerException e) {
+                }
+
+                try {
+                    customerIDField.setText(result.getString("customer_customernr"));
+                } catch (NullPointerException e) {
+                }
+
+                try {
+                    luggageTypeField.setText(result.getString("luggage_type"));
+                } catch (NullPointerException e) {
                 }
             }
         } catch (SQLException ex) {
@@ -143,10 +179,80 @@ public class DatabaseEdit implements Initializable {
         }
     }
 
-    @FXML
+    private void setupAiportBox() {
+        try {
+            // Do an SQL query to get all airports
+            String query = "SELECT IATA FROM airport;";
+            ResultSet result = MainApp.myJDBC.executeResultSetQuery(query);
+            // Loop over de resultset
+            while (result.next()) {
+                // Voeg de individuele results to aan de database
+                Airports.add(result.getString("IATA"));
+            }
+            // Set the items of the airportbox to the observable list
+            airportField.setItems(Airports);
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountbeheerFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     private void closeStage(ActionEvent event) {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
+    }
+
+    @FXML
+    private void commitChanges(ActionEvent event) {
+        String query = "UPDATE `luggagem`.`luggage` "
+                + "SET `flightnr`='?', "
+                + "`labelnr`='?', "
+                + "`destination`='?', "
+                + "`luggage_type`='?', "
+                + "`brand`='?', "
+                + "`location_found`='?', "
+                + "`primary_color`='?', "
+                + "`secondary_color`='?', "
+                + "`notes`='?', `size`='?', "
+                + "`weight`='?', "
+                + "`customer_firstname`='?', "
+                + "`customer_preposition`='?', "
+                + "`customer_lastname`='?', "
+                + "`airport_IATA`='?', "
+                + "`customer_customernr`='?'"
+                + " WHERE `registrationnr`='?';";
+        String luggageSize = luggageHeightField.getText() + "x" + luggageWidthField.getText() + "x" + luggageDepthField.getText();
+        
+        try {
+            PreparedStatement ps = null;
+            Connection conn = MainApp.myJDBC.getConnection();
+            conn.setAutoCommit(false);
+            ps = conn.prepareStatement(query);
+            ps.setString(1, flightNrField.getText());
+            ps.setString(2, labelNrField.getText());
+            ps.setString(3, destinationField.getText());
+            ps.setString(4, luggageTypeField.getText());
+            ps.setString(5, brandField.getText());
+            ps.setString(6, locationFoundField.getText());
+            ps.setString(7, color1Field.getText());
+            ps.setString(8, colorField2.getText());
+            ps.setString(9, notesField.getText());
+            ps.setString(10, luggageSize);
+            ps.setString(11, weightField.getText());
+            ps.setString(12, firstNameField.getText());
+            ps.setString(13, betweenNameField.getText());
+            ps.setString(14, lastNameField.getText());
+            ps.setString(15, airportField.getValue());
+            ps.setString(16, customerIDField.getText());
+            ps.setInt(17, registrationNumber);
+            ps.executeUpdate();
+            conn.commit();
+
+        } catch (SQLException e) {
+            System.err.print("SQL error setfields: @@@@@@ " + e);
+        }
+
+        MainApp.myJDBC.executeUpdateQuery(query);
+        closeStage(event);
     }
 
 }
